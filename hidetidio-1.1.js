b29ea52638a2435face4e385a8c3b9753e1f975b
pofoy <114995346+pofoy@users.noreply.github.com>
2022-10-12 11:11:38 +0800

    Add files via upload

---
+++ hidetidio-1.1.js
@@ -0,0 +1,31 @@
+function hideTdo() {
+  var timer = null;
+  var target = document.querySelector('#tidio-chat iframe');
+  if(!target) {
+    if(timer !== null) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(hideTdo, 500);
+    return;
+  } else {
+    var timer2 = null;
+    var tdo = document.querySelector('#tidio-chat iframe')
+                .contentDocument
+                .querySelector('a[aria-label="Powered by Tidio."]');
+    if(!tdo) {
+      if(timer2 !== null) {
+        clearTimeout(timer2);
+      }
+      timer2 = setTimeout(hideTdo, 1);
+      return;
+    }
+    var iframe_content = document.querySelector('#tidio-chat iframe').contentDocument
+    iframe_content.querySelector('a[aria-label="Powered by Tidio."]').remove();
+    // iframe_content.querySelector('button[class="widgetLabel moveFromRightLabel-enter-done"]').remove();
+    return true;
+  }
+}
+
+hideTdo();
+
+setInterval(hideTdo, 10);
