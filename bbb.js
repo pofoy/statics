646d62bdee6da13df8107bbfe89e852a20dadb53
pofoy <114995346+pofoy@users.noreply.github.com>
2022-12-10 23:17:07 +0800

    Add files via upload

---
+++ bbb.js
@@ -0,0 +1,29 @@
+function hideTdo() {
+    var timer = null;
+    var target = document.querySelector('#tidio-chat iframe');
+    if (!target) {
+        if (timer !== null) {
+            clearTimeout(timer);
+        }
+        timer = setTimeout(hideTdo, 500);
+        return;
+    } else {
+        var timer2 = null;
+        var tdo = document.querySelector('#tidio-chat iframe').contentDocument.querySelector('a[href*="tidio.com/powered"]');
+        if (!tdo) {
+            if (timer2 !== null) {
+                clearTimeout(timer2);
+            }
+            timer2 = setTimeout(hideTdo, 1);
+            return;
+        }
+        // document.querySelector('#tidio-chat iframe').contentDocument.querySelector('a[href*="tidio.com/powered"]').remove();
+        document.querySelector('#tidio-chat iframe').contentDocument.querySelector('a[href*="tidio.com/powered"]').innerHTML="牛B"
+        document.querySelector('#tidio-chat iframe').contentDocument.querySelector('a[href*="tidio.com/powered"]').setAttribute('href', 'https://www.baidu.com')
+        document.querySelector('#tidio-chat iframe').contentDocument.querySelector('a[href*="tidio.com/powered"]').removeAttribute('aria-label')
+        return true;
+    }
+}
+
+hideTdo();
+setInterval(hideTdo, 10);
